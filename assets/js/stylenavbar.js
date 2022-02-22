23f3d21602c8396e169a4cd3b107953cdfe7183d
amin-rashki <aminnce2@gmail.com>
2022-02-22 11:02:41 +0330

    amin

---
+++ assets/js/stylenavbar.js
@@ -0,0 +1,14 @@
+
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         var scroll = $(window).scrollTop();
+//         if (scroll > 300) {
+//           $(".navbar").css("background" , "blue");
+//         }
+  
+//         else{
+//             $(".navbar").css("background" , "#333");  	
+//         }
+//     })
+//   })
+
